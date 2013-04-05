df680f1f02cccd96b80ddb210a75d14e232c95ca
vincent <vincent@yandex-team.ru>
2013-04-05 20:28:54 +0400

    Q learning

--- genetics/src/main/java/genetics/HiffRL.java
+++ genetics/src/main/java/genetics/HiffRL.java
@@ -6,6 +6,8 @@ import genetics.hiff.MutationOperator;
 import genetics.hiff.Population;
 import org.uncommons.maths.random.MersenneTwisterRNG;
 
+import java.util.ArrayList;
+
 /**
  * @author Vladislav Kononov vincent@yandex-team.ru
  */
@@ -20,5 +22,29 @@ public class HiffRL {
         Population p = new Population(POPULATION_SIZE, INDIVIDUAL_SIZE);
         CrossoverOperator mutation = new CrossoverOperator(p, RNG);
         MutationOperator mutation2 = new MutationOperator(p, RNG);
+        ArrayList<ArrayList<Double>> Q = new ArrayList<ArrayList<Double>>();
+        ArrayList<ArrayList<Double>> P = new ArrayList<ArrayList<Double>>();
+        for (int i = 0; i < 2; ++i) {
+            Q.add(new ArrayList<Double>());
+            P.add(new ArrayList<Double>());
+        }
+        for (int i = 0; i < 2; ++i){
+            for (int k = 0; k < 2; ++k) {
+                Q.get(i).add(0.0);
+                P.get(i).add(0.5);
+            }
+        }
+        for (int i = 0; i < 10; ++i){
+            int state = RNG.nextInt() % 2;
+            for (int k = 0; k < 10; ++k){
+                double probability = RNG.nextDouble();
+                double sum = 0;
+                int newState = -1;
+                for (int z = 0; sum < probability; ++z){
+                    sum += P.get(state).get(z);
+                    newState = z;
+                }
+            }
+        }
     }
 }
